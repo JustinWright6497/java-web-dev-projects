06e86a0698070d1f999db27fd18d6ade5fc26f27
Justin Wright <djw6497@gmail.com>
2023-09-07 20:54:00 -0500

    Completed classes part 2 studio

--- classes-part-2/studio/src/main/java/org/launchcode/Main.java
+++ classes-part-2/studio/src/main/java/org/launchcode/Main.java
@@ -1,8 +1,35 @@
 package org.launchcode;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
+        MenuItem menuItem1 = new MenuItem("Spaghetti",8.00,"Noodles with Sauce","Main Course", true);
+        MenuItem menuItem2 = new MenuItem("Gyoza",2.50,"Pork Filled Dumplings","Appetizer", true);
+        MenuItem menuItem3 = new MenuItem("Gyro",9.75,"Pita Pocket like hot pocket but better","Main Course", true);
+        MenuItem menuItem4 = new MenuItem("Apple Pie",2.25,"Apple with Pie Crust","Dessert", true);
+
+        Menu menu1 = new Menu(LocalDate.now(), new ArrayList<MenuItem>());
+
+        menu1.addItem(menuItem2);
+        menu1.addItem(menuItem1);
+        menu1.addItem(menuItem3);
+        menu1.addItem(menuItem4);
+
+        menu1.getItems();
+
+        menu1.getItem();
+
+        menu1.removeItem(menuItem4);
+
+        menu1.getItems();
+
+        MenuItem menuItem5 = new MenuItem("Gyro",9.75,"Pita Pocket like hot pocket but better","Main Course", true);
+
+        menu1.addItem(menuItem5);
+
+        menu1.getItems();
     }
 }
--- classes-part-2/studio/src/main/java/org/launchcode/MenuItem.java
+++ classes-part-2/studio/src/main/java/org/launchcode/MenuItem.java
@@ -1,16 +1,38 @@
 package org.launchcode;
 
 public class MenuItem {
+    private String name;
     private double price;
     private String description;
     private String category;
     private boolean isNew;
 
-    public MenuItem(double p, String d, String c, boolean iN) {
+    public MenuItem(String n, double p, String d, String c, boolean iN) {
+        this.name = n;
         this.price = p;
         this.description = d;
         this.category = c;
         this.isNew = iN;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getCategory() {
+        return category;
     }
 
     public void setPrice(double price) {
@@ -26,7 +48,32 @@ public class MenuItem {
     }
 
     public void setNew(boolean aNew) {
-        isNew = aNew;
+        this.isNew = aNew;
+    }
+
+    public boolean getNew() {
+        return this.isNew;
+    }
+
+    @Override
+    public String toString() {
+        return "Menu Item: " + this.name + ".\nPrice: " + this.price + ".\nDescription: " + this.description + ".\nCategory: " + this.category + ".\nIs New?: " + this.isNew + "\n\n";
+    }
+
+    public boolean equals(Object toBeCompared) {
+        if (toBeCompared == this) {
+            return true;
+        }
+
+        if (toBeCompared == null) {
+            return false;
+        }
+
+        if (toBeCompared.getClass() != getClass()) {
+            return false;
+        }
+
+        MenuItem menuItem = (MenuItem) toBeCompared;
+        return menuItem.getPrice() == getPrice();
     }
 }
-
